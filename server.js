b9915dd2b1c56ab25fc2447aaec5204023ed1d4f
Ernesto Ledesma <erneledesma@gmail.com>
2018-09-17 21:17:42 -0300

    primer commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const axios = require('axios'); // declarar axios
+
+const argv = require('yargs').options({
+  direccion:{
+    alias: 'd',
+    desc: 'Direccion de la ciudad para obtener el clima',
+    demand: true
+  }
+}).argv;
+
+
+let encodedUrl = encodeURI(argv.direccion) // encodeURI se utiliza para escapar a los espacios de la direccion
+axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodedUrl}+CA&key=AIzaSyD5Z9NYEynycmMZ5HajTr45VU88J-rEzG0`)
+      .then( resp => {
+
+        let location = resp.data.results[0];
+        let coors = location.geometry.location;
+        //
+        console.log('Direccion:', location.formatted_address);
+        console.log('lat',coors.lat);
+        console.log('lng',coors.lng);
+
+        //console.log(resp.data);
+        //console.log(JSON.stringify(resp.data,undefined,2));
+         console.log(resp.status);
+      })
+      .catch(err => console.log('ERROR!!', err));
